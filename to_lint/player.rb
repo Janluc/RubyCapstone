0701bf89271afa68e4bab37b7254f9cbd3140bcc
Janluc <Janlucsaneaux@gmail.com>
2020-12-02 19:56:49 -0400

    update player test case

--- to_lint/player.rb
+++ to_lint/player.rb
@@ -5,8 +5,10 @@ class Player
   attr_accessor :name, :token
 
   def initialize(name, token
-    self.name = name
+  self.name = name
     self.token = token
+    |unclosed pipe
+    {unclosed bracket
   end
 end
 
