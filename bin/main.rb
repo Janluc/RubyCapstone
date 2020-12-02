352edc2d597b3446074c0b6773d42b14b18e69b1
Janluc <Janlucsaneaux@gmail.com>
2020-12-02 18:32:26 -0400

    reformat classes and seperate clases into their own files

--- bin/main.rb
+++ bin/main.rb
@@ -12,17 +12,24 @@ def print_bracket_pair(line, line_number, bracket_pair)
   puts ''
 end
 
+def print_indentation(line, line_number)
+  puts "Incorrect indentation on line #{line_number + 1}"
+  puts "Line #{line_number + 1}: #{line} <-"
+  puts ''
+end
+
 file = File.open('C:/Users/Janluc/Desktop/Programming/Microverse/Ruby/TestFiles/lib/player.rb')
 
 linter = Linter.new(file)
+bracket_linter = linter.bracket_linter
+indentation_linter = linter.indentation_linter
 
 linter.file_lines.each_with_index do |line, line_number|
   print_whitespace(line, line_number) if linter.trailing_whitespace?(line)
-  linter.check_indentation(line)
-  next unless linter.missing_bracket_pair?(line)
+  print_indentation(line, line_number) if indentation_linter.check_indentation(line)
+  next unless linter.missing_brackets?(line)
 
-  pair_index = linter.bracket_pair
-  name_of_bracket_pair = linter.bracket_pairs[pair_index].name
+  name_of_bracket_pair = bracket_linter.bracket_pairs[bracket_linter.pair_index].name
   print_bracket_pair(line, line_number, name_of_bracket_pair)
 end
 
